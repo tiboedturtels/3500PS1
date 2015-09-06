c350fba196bd1827080a9ebe6d6467c8079ec9bd
tiboedturtels <u0927597@utah.edu>
2015-09-06 16:06:07 -0600

    Added Operate method and got EvaluatorTester to run.

--- FormulaEvaluator/EvaluatorTester/Program.cs
+++ FormulaEvaluator/EvaluatorTester/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FormulaEvaluator;
+
 
 namespace EvaluatorTester
 {
@@ -10,6 +12,8 @@ namespace EvaluatorTester
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Evaluator.Operate(1.0, 2.0, "/"));
+            Console.Read();
         }
     }
 }
--- FormulaEvaluator/FormulaEvaluator/Class1.cs
+++ FormulaEvaluator/FormulaEvaluator/Class1.cs
@@ -13,7 +13,7 @@ namespace FormulaEvaluator
 
 
         /// <summary>
-        /// 
+        /// Evaluates an an infix expression.
         /// </summary>
         /// <param name="exp"> A string representing the expression to be evaluated. </param>
         /// <param name="variableEvaluator"> Delegate method which will return the int value of a variable. </param>
@@ -23,16 +23,47 @@ namespace FormulaEvaluator
             Stack<double> nums = new Stack<double>();
             Stack<String> operators = new Stack<string>();
 
-            string[] substrings = Regex.Split(s, exp);
+            string[] substrings = Regex.Split("", exp);
 
             for(int i = 0; i < substrings.Length; i++)
             {
                 Double Result;
                 if(Double.TryParse(substrings[i], out Result))
                 {
-                    if()
+                    if (operators.Count != 0 && (operators.Peek() == "*" || operators.Peek() == "/"))
+                        nums.Push(Operate(nums.Pop(), Result, operators.Pop()));
+
+                    else
+                        nums.Push(Result);
                 }
             }
+
+            return -1;
         }
+
+        /// <summary>
+        /// Completes a single operation on two Doubles
+        /// </summary>
+        /// <param name="num1"> The first operand </param>
+        /// <param name="num2"> The second operand </param>
+        /// <param name="operation"> The operation to be performed </param>
+        /// <returns> The Double value of the evaluated operation </returns>
+        public static Double Operate(Double num1, Double num2, String operation)
+        {
+            if (operation == "*")
+                return num1 * num2;
+
+            if (operation == "/")
+                return num1 / num2;
+
+            if (operation == "+")
+                return num1 + num2;
+
+            if (operation == "-")
+                return num1 - num2;
+
+            return -1.0;
+        }
+
     }
 }
